3181db7f887072a57bcd49f7504618c97b256496
denah224 <dhowpun@sfsu.edu>
2026-03-18 13:39:27 -0700

    Add image loading and display in sketch.js
    Load two images in preload and display them in setup. testing this tho

--- Project 1/sketch.js
+++ Project 1/sketch.js
@@ -1,3 +1,17 @@
+let myImage1;
+let myImage2;
+
+function preload(){
+  myImage1 = loadImage ('cat.jpeg');
+  myImage2 = loadImage ('cat2.jpeg');
+}
+
+function setup() {
+  createCanvas(800, 500);
+  image(myImage1, 50,50);
+  image(myImage2, 100,100);
+}
+
 function setup() {
   createCanvas(800, 500);
 }
